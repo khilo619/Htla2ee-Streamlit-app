e4a894d7b707edbfafe88b26fcf8578492e3c69a
Khaled Nabil Fathy Mohamed <KhaLood619@users.noreply.huggingface.co>
2025-09-28 22:25:02 +0000

    Update app.py

--- app.py
+++ app.py
@@ -120,7 +120,7 @@ st.markdown("""
 # Load data
 @st.cache_data
 def load_data():
-    df = pd.read_excel('data/processed/cleaned_data.csv')
+    df = pd.read_csv('data/processed/cleaned_data.csv')
     return df
 
 # Helper function to encode features
